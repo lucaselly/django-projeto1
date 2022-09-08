37e571bdcc8159b6f93e9dc6968a00ddf743a852
Lucas Elly <lucasalvesferraz@gmail.com>
2022-09-07 21:47:03 -0300

    migrate

--- recipes/models.py
+++ recipes/models.py
@@ -1,11 +1,14 @@
-from pickle import FALSE
-
+from django.contrib.auth.models import User
 from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
 
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
-    description = models.charField(max_length=165)
+    description = models.CharField(max_length=165)
     slug = models.SlugField()
     preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
@@ -15,5 +18,9 @@ class Recipe(models.Model):
     preparation_steps_is_html = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(defautl=False)
+    is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True)
