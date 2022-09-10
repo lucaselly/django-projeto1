a09123f128a95f7f7f5bc87fbdc9a8af429a1849
Lucas Elly <lucasalvesferraz@gmail.com>
2022-09-09 22:39:52 -0300

    404 na home

--- recipes/views.py
+++ recipes/views.py
@@ -5,9 +5,11 @@ from recipes.models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.filter(
-        is_published=True
-    ).order_by('-id')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            is_published=True,
+        ).order_by('-id')
+    )
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
